a4c9a61f8984dd80e8c61e70b1c6ae593e241052
Paul Gier <pgier@redhat.com>
2019-09-19 13:58:59 -0500

    Initial configuration for running prometheus on minikube

---
+++ kubernetes-config/commands.sh
@@ -0,0 +1,23 @@
+
+# start minikube with kvm driver (for Fedora based systems)
+# minikube --vm-driver=kvm2
+#
+# enable minikube metrics-server
+# minikube addons enable metrics-server
+
+alias k=kubectl
+
+# Create the prometheus configmap
+k apply -f prometheus-config.yml
+
+# Deploy Prometheus
+k apply -f prometheus.yml
+
+# Attach local port to prometheus-service.  Run in the background
+# so that we can still run commands.
+# This can be killed with `fg` followed by `ctrl-c`
+# or something like `pkill kubectl`
+k port-forward svc/prometheus-service 9090:9090 > /dev/null &
+
+
+
